c392ff9418c9c60fa136f4e4d1f891b70c8a6b96
Robert DJ <git@dahl-jacobsen.dk>
2014-12-08 22:13:55 +0100

    Integration tests
    Tests passed for multiple combinations of dimensions and orders

--- test/quadtest.jl
+++ test/quadtest.jl
@@ -1,28 +1,62 @@
-using Base.Test
-
-# Exact computation of moments of the standard normal distribution
-function normmoment( P::Int )
-	if P == 0
-		return 1.0
-	elseif isodd(P)
-		return 0.0
-	else
-		return float(prod( [1:2:P-1] ))
-	end
-
-end
-
-# Exact computation of moments of the multidimensional standard normal distribution
-# P : Vector of moments for each dimension
-function normmoment( P::Vector{Int} )
+# Moments of exp(-|x|^2)
+function ghmoment( P::Vector )
+	P = float(P)
 	D = length(P)
-	M = 1
+
+	I = 1.0
 
 	for d = 1:D
-		I = normmoment( P[d] )
-		M *= I[1]
+		Id = quadgk( x -> x.^P[d] .* exp(-x.^2), -Inf, Inf )
+		I *= Id[1]
 	end
 
-	return M
-
+	return I 
 end
+
+function normquad( P::Vector, nodes::Matrix, weights::Vector )
+	# Evaluate integrand
+	F = broadcast( ^, nodes, P )
+	I = vec(prod(F,1))
+
+	# Evaluate quadrature sum
+	Q = dot( I, weights )
+
+	return Q
+end
+
+
+# ------------------------------------------------------------
+# Test quadrature for various dimensions and orders
+# Note: It is not interesting to test uneven moments for Gauss-Hermite
+
+# Dimension and order
+D = 3
+order = 4
+
+# Quadrature points and weights
+N, W = smolyak( D, order )
+
+# Moments to test
+vecs = repmat( Any[[0:order]], D )
+P = combvec( vecs )
+
+M = size(P, 2)
+for m = 1:M
+	# Test if all moments are even
+	curP = P[:,m]
+
+	if all(map(iseven, curP))
+		#@show curP
+
+		# Expected test result depends on the total degree
+		I = ghmoment(curP) 
+		Q = normquad(curP, N, W)
+
+		if sum(curP) <= 2*order-1
+			@test_approx_eq I Q
+		else
+			@test abs(I - Q) > 1e-3
+		end
+	end
+end
+
